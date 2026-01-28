e8dfacb62b01767d4c26b4cd2ee3c6cbd72f51e0
Kento Okura <kentookura@gmail.com>
2026-01-28 13:35:41 +0100

    formatting

--- lib/frontend/Forester.ml
+++ lib/frontend/Forester.ml
@@ -120,9 +120,19 @@ let outputs_for_article ~(forest : State.t) (article : _ T.article) =
     let html_content =
       Pure_html.to_string @@ Html_client.render_page ~forest article
     in
-    let debug_route = URI.with_path_components (URI.append_path_component (URI.path_components uri) "index.tree") uri in
-    let debug_content = Format.asprintf "%a" Types.(pp_article pp_content) article in
-    [xml_route, xml_content; html_route, html_content; debug_route, debug_content;]
+    let debug_route =
+      URI.with_path_components
+        (URI.append_path_component (URI.path_components uri) "index.tree")
+        uri
+    in
+    let debug_content =
+      Format.asprintf "%a" Types.(pp_article pp_content) article
+    in
+    [
+      (xml_route, xml_content);
+      (html_route, html_content);
+      (debug_route, debug_content);
+    ]
 
 let outputs_for_asset (asset : T.asset) =
   let route = asset.uri in
